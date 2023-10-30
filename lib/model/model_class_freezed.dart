c9ddf6c64e1f8c0f6c98aae584c66935850e5097
nikhilmkda <nikhilk9567@gmail.com>
2023-10-30 16:47:50 +0530

    freezed model class added successfully

--- lib/model/model_class_freezed.dart
+++ lib/model/model_class_freezed.dart
@@ -1,59 +1,57 @@
-// import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'model_class_freezed.freezed.dart';
 
-// part 'temperatures.freezed.dart';
-// part 'temperatures.g.dart'; // Add this line for JSON serialization
+@freezed
+class Temperatures with _$Temperatures {
+  const factory Temperatures({
+    required int id,
+    required String name,
+    required String username,
+    required String email,
+    required Address address,
+    required String phone,
+    required String website,
+    required Company company,
+  }) = _Temperatures;
 
-// @freezed
-// abstract class Temperatures with _$Temperatures {
-//   const factory Temperatures({
-//     required int id,
-//     required String name,
-//     required String username,
-//     required String email,
-//     required Address address,
-//     required String phone,
-//     required String website,
-//     required Company company,
-//   }) = _Temperatures;
+  factory Temperatures.fromJson(Map<String, dynamic> json) =>
+      _$TemperaturesFromJson(
+          json); // Add this constructor for JSON deserialization
+}
 
-//   factory Temperatures.fromJson(Map<String, dynamic> json) =>
-//       _$TemperaturesFromJson(
-//           json); // Add this constructor for JSON deserialization
-// }
+@freezed
+abstract class Address with _$Address {
+  const factory Address({
+    required String street,
+    required String suite,
+    required String city,
+    required String zipcode,
+    required Geo geo,
+  }) = _Address;
 
-// @freezed
-// abstract class Address with _$Address {
-//   const factory Address({
-//     required String street,
-//     required String suite,
-//     required String city,
-//     required String zipcode,
-//     required Geo geo,
-//   }) = _Address;
+  factory Address.fromJson(Map<String, dynamic> json) =>
+      _$AddressFromJson(json); // Add this constructor for JSON deserialization
+}
 
-//   factory Address.fromJson(Map<String, dynamic> json) =>
-//       _$AddressFromJson(json); // Add this constructor for JSON deserialization
-// }
+@freezed
+abstract class Geo with _$Geo {
+  const factory Geo({
+    required String lat,
+    required String lng,
+  }) = _Geo;
 
-// @freezed
-// abstract class Geo with _$Geo {
-//   const factory Geo({
-//     required String lat,
-//     required String lng,
-//   }) = _Geo;
+  factory Geo.fromJson(Map<String, dynamic> json) =>
+      _$GeoFromJson(json); // Add this constructor for JSON deserialization
+}
 
-//   factory Geo.fromJson(Map<String, dynamic> json) =>
-//       _$GeoFromJson(json); // Add this constructor for JSON deserialization
-// }
+@freezed
+abstract class Company with _$Company {
+  const factory Company({
+    required String name,
+    required String catchPhrase,
+    required String bs,
+  }) = _Company;
 
-// @freezed
-// abstract class Company with _$Company {
-//   const factory Company({
-//     required String name,
-//     required String catchPhrase,
-//     required String bs,
-//   }) = _Company;
-
-//   factory Company.fromJson(Map<String, dynamic> json) =>
-//       _$CompanyFromJson(json); // Add this constructor for JSON deserialization
-// }
+  factory Company.fromJson(Map<String, dynamic> json) =>
+      _$CompanyFromJson(json); // Add this constructor for JSON deserialization
+}
